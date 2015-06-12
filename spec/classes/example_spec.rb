1e05e8082db1c459c877db7a4fa0e389010344b7
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-12 14:53:42 +0200

    add test on dependencies

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -13,10 +13,11 @@ describe 'profile_puppetmaster' do
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('profile_puppetmaster') }
-          it { is_expected.to contain_apt__source('puppetlabs') }
-          it { is_expected.to contain_class('puppetdb') }
-          it { is_expected.to contain_package('puppetmaster-passenger') }
           it { is_expected.to contain_host( facts[:fqdn] )}
+          it { is_expected.to contain_apt__source('puppetlabs') }
+          it { is_expected.to contain_package('puppetmaster-passenger').that_requires('Apt::source[puppetlabs]') }
+          it { is_expected.to contain_class('puppetdb').that_requires('Apt::source[puppetlabs]') }
+          it { is_expected.to contain_class('puppetdb::master::config').that_requires('Apt::source[puppetlabs]') }
          
         end
       end

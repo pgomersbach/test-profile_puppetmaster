ee1648c0ddaa10de04eb0b4c81cd2ec55b37d8ad
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-16 20:28:23 +0200

    change repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,10 +19,10 @@ class profile_puppetmaster
 
   apt::source { 'puppetlabs':
     location => 'http://apt.puppetlabs.com',
-    key      => {
-      id     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-      server => 'pgp.mit.edu',
-    },
+#    key      => {
+#      id     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+#      server => 'pgp.mit.edu',
+#    },
   }
 
   package { 'puppetmaster-passenger':

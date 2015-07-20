aa1ee3ace2fa9492da4396eb412ebc87f47475c6
Paul Gomersbach <p.gomersbach@rely.nl>
2015-07-20 12:49:52 +0200

    change to default 8080 puppetdb port

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,6 @@ class profile_puppetmaster
 
   class { 'puppetdb':
     listen_address => '0.0.0.0',
-    listen_port    => '9080',
     confdir        => '/etc/puppetdb/conf.d',
     require        => [ Apt::Source['puppetlabs'], Host[ $::fqdn ] ],
   }
